6d473bfb681d39f8ee9993ef28074f801941785c
wangq <wangqing@nianhuatech.com>
2023-04-27 17:09:31 +0800

    主页以及主页图标

--- src/components/AppFrame/AppHeader/AppHeader.js
+++ src/components/AppFrame/AppHeader/AppHeader.js
@@ -2,6 +2,7 @@
 import React from "react";
 import './AppHeader.css';
 import {Layout} from "antd";
+import {LinkedinOutlined} from "@ant-design/icons";
 
 /**
  * 项目 - 主框架 - 顶部
@@ -19,7 +20,7 @@ export default class AppHeader extends React.Component {
     return (
       <Layout.Header className={'app-frame-header'}>
         <div className={'header-logo'}>
-          INTERESTING APP
+          <LinkedinOutlined className={'header-logo-icon'}/>INTERESTING APP
         </div>
         <div className={'header-page-route-other'}>
           Other
